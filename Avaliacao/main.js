21d5a5110c411ca3348da8a8b2f2e63f807fb31a
Pedro Lessa <pepeulessa@hotmail.com>
2023-03-04 13:23:32 -0300

    Projeto Final

--- Avaliacao/main.js
+++ Avaliacao/main.js
@@ -1,13 +1,37 @@
 const listaNotas = document.querySelectorAll('.item-lista-notas')
+const listaNotasBotoes = document.querySelectorAll('.botao-lista')
+const submit = document.querySelector('.botao-enviar')
 
-listaNotas.forEach((elemento)=>{
-    console.log(elemento.length)
-    elemento.addEventListener('click', (evento)=>{
-        const nota= evento.target.defaultValue
-        console.log(listaNotas)
+const paginaAvaliacao = document.querySelector('.avaliacao')
+const paginaObrigado = document.querySelector('.obrigado')
 
-        
-        // evento.target.className = "botao-lista-onclick"
-    
+const valorNota = document.querySelector('.valor-nota')
+const totalNota = document.querySelector('.total-nota')
+
+    listaNotas.forEach((elemento)=>{
+        elemento.addEventListener('click', (evento)=>{
+            nota = evento.target.defaultValue
+            
+            listaNotasBotoes.forEach((elemento)=>{
+                if(elemento.classList.length === 2){
+                    elemento.classList.remove('ativo')
+                }
+                else{
+                    evento.target.classList.add('ativo')
+                }
+            })
+            
+        })
     })
-})
+
+    submit.addEventListener('click', ()=>{
+        recarregaPagina()
+    })
+
+    function recarregaPagina(){
+        paginaAvaliacao.style.display = 'none';
+        paginaObrigado.style.display = 'block';
+
+        valorNota.innerText = nota
+        totalNota.innerHTML = listaNotas.length
+    }
